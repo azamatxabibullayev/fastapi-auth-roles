1c3f67d7216a83f9930c38bebf043bcb7fd45ed1
azamatxabibullayev <azamatxabibullayev02@gmail.com>
2025-10-21 15:17:50 +0500

    env file updated

--- core/config.py
+++ core/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Settings:
     PROJECT_NAME: str = "FastAPI Auth Roles"
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/auth_db")
-    JWT_SECRET: str = os.getenv("JWT_SECRET", "supersecret")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
